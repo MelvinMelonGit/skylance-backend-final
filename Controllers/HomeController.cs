8500350c5d65f4b92d393c7128a5e802f8967c07
MelvinMelonGit <83698963+MelvinMelonGit@users.noreply.github.com>
2025-08-08 16:45:18 +0800

    Updated deploy.yml
    Updated deploy.yml

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace skylance_backend.Controllers;
+
+[ApiController]
+[Route("/")]
+public class HomeController : Controller
+{
+    [HttpGet]
+    public IActionResult GetHello()
+    {
+        return Ok(new { message = "Hello world!" });
+    }
+}
